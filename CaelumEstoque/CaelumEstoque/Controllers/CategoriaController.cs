7716a426c53040cd9ca7ba6c7eb47e02da8fd08a
Eric Fratantonio <er.frata@gmail.com>
2017-11-30 09:31:41 -0200

    Final
    The end of the Alura - ASP.NET course.

---
+++ CaelumEstoque/CaelumEstoque/Controllers/CategoriaController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CaelumEstoque.DAO;
+using CaelumEstoque.Models;
+
+namespace CaelumEstoque.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        CategoriasDAO dao = new CategoriasDAO(); 
+
+        // GET: Categoria
+        public ActionResult Index()
+        {
+            IList<CategoriaDoProduto> categorias = dao.Lista();
+            ViewBag.Categorias = categorias;
+            return View();
+        }
+
+        
+        public ActionResult Form()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult Adiciona(CategoriaDoProduto Categoria)
+        {
+            CategoriasDAO dao = new CategoriasDAO();
+            dao.Adiciona(Categoria);
+
+            return RedirectToAction("Index", "Categoria");
+        }
+    }
+}
---
+++ CaelumEstoque/CaelumEstoque/Controllers/ProdutoController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CaelumEstoque.DAO;
+using CaelumEstoque.Models;
+using CaelumEstoque.Filtro;
+
+namespace CaelumEstoque.Controllers
+{
+    [AutorizacaoFilter]
+
+    public class ProdutoController : Controller
+    {
+
+        [Route("produtos", Name ="ListaProdutos")]
+        // GET: Produto
+        public ActionResult Index()
+        {
+            
+            ProdutosDAO dao = new ProdutosDAO();
+            IList<Produto> produtos = dao.Lista();
+            return View(produtos);
+        }
+
+        public ActionResult Form()
+        {
+            ViewBag.Produto = new Produto();
+            CategoriasDAO CategoriaDao = new CategoriasDAO();
+            IList<CategoriaDoProduto> categorias = CategoriaDao.Lista();
+            ViewBag.Categorias = categorias;
+            
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Adiciona(Produto produto)
+        {
+            int idDaInformatica = 1;
+
+            if(produto.CategoriaId.Equals(idDaInformatica) && produto.Preco < 100)
+            {
+                ModelState.AddModelError("produto.Invalido","Informática com preço abaixo de 100 reais");
+            }
+
+            if (ModelState.IsValid)
+            {
+                ProdutosDAO dao = new ProdutosDAO();
+                dao.Adiciona(produto);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.Produto = produto;
+                CategoriasDAO dao = new CategoriasDAO();
+                ViewBag.Categorias = dao.Lista();
+                return View("Form");
+            }
+
+        }
+
+        [Route("produtos/{id}", Name = "VisualizaProduto")]
+
+        public ActionResult Visualiza (int id)
+        {
+            ProdutosDAO dao = new ProdutosDAO();
+            Produto produto = dao.BuscaPorId(id);
+            ViewBag.Produto = produto;
+            return View(); 
+        }
+
+        public ActionResult DecrementaQtd(int id)
+        {
+            ProdutosDAO dao = new ProdutosDAO();
+            Produto produto = dao.BuscaPorId(id);
+            produto.Quantidade--;
+            dao.Atualiza(produto);
+            return Json(produto);
+        }
+    }
+}
